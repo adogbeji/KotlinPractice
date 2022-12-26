00a7d4425ce8820201897e124a7797e4ca041d24
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2022-12-26 22:08:08 +0000

    Practise inheritance & interfaces

--- app/src/main/java/org/benjaminakponasa/kotlinpractice/Basics.kt
+++ app/src/main/java/org/benjaminakponasa/kotlinpractice/Basics.kt
@@ -3,3 +3,22 @@ package org.benjaminakponasa.kotlinpractice
 fun main() {
     println("I'm an Android App Developer!")
 }
+
+// Interfaces
+interface MotorInterface {
+    var topSpeed: Double
+    fun drive(): String
+    fun brake() {
+        println("Vehicle braking")
+    }
+}
+
+// Inheritance
+open class Motor {
+    // Properties
+    // Methods
+}
+
+open class MotorOne: Motor() {}
+
+class MotorTwo: MotorOne() {}
