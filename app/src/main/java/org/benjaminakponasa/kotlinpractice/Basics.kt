90384b4df7d7703541813a0d7af18a4ca7f32647
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2022-12-28 21:35:56 +0000

    Practise arrays, lists, inheritance & interfaces

--- app/src/main/java/org/benjaminakponasa/kotlinpractice/Basics.kt
+++ app/src/main/java/org/benjaminakponasa/kotlinpractice/Basics.kt
@@ -2,6 +2,15 @@ package org.benjaminakponasa.kotlinpractice
 
 fun main() {
     println("I'm an Android App Developer!")
+
+
+    // When Expressions
+//    val name = "Chris"
+//
+//    when (name) {
+//        "Tom" -> println("$name works as a Lawyer!")
+//        "Michael" -> println("$name works as a Chef!")
+//    }
 }
 
 // Interfaces
@@ -14,9 +23,9 @@ interface MotorInterface {
 }
 
 // Inheritance
-open class Motor (val model: String, val colour: String, val doors: Int) {
+open class Motor(val model: String, val colour: String, val doors: Int) {
     // Properties
     // Methods
 }
 
-class MotorOne(model: String, colour: String, doors: Int): Motor(model, colour, doors) {}
+class MotorOne(model: String, colour: String, doors: Int, owner: String): Motor(model, colour, doors) {}
